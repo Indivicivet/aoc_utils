244ecbc588a7bb8b3c514dbd38c8357d42dc95cc
indi <indivicivet@gmail.com>
2024-12-05 20:37:50 +0000

    add getattr to aoc_utils (boilerplate)

--- src/aoc_utils.py
+++ src/aoc_utils.py
@@ -2,3 +2,9 @@ from pathlib import Path
 
 _data_folder = Path(__file__).parent.parent / "data"
 _data_folder.mkdir(exist_ok=True, parents=True)
+
+
+def __getattr__(name):
+    if name and name[0] == "y":
+        return f"year_{name}"
+    raise AttributeError("Not a year")
