d98c7288460c4691e0729ad3d9f583dea2409bd8
indi <indivicivet@gmail.com>
2024-12-05 20:39:46 +0000

    add yearloader boilerplate

--- src/aoc_utils.py
+++ src/aoc_utils.py
@@ -4,7 +4,15 @@ _data_folder = Path(__file__).parent.parent / "data"
 _data_folder.mkdir(exist_ok=True, parents=True)
 
 
+class YearLoader:
+    def __init__(self, year):
+        self.year = year
+
+
 def __getattr__(name):
     if name and name[0] == "y":
-        return f"year_{name}"
+        try:
+            return YearLoader(int(name[1:]))
+        except Exception as e:
+            raise Exception("unknown year") from e
     raise AttributeError("Not a year")
